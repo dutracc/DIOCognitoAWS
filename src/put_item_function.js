4f93833b11555892d178bfe071048fec56cd864d
dutracc <86779358+dutracc@users.noreply.github.com>
2023-05-30 23:34:36 -0300

    Update put_item_function.js

--- src/put_item_function.js
+++ src/put_item_function.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
     
     const params = {
       TableName : 'Items',
-      /* Item properties will depend on your application concerns */
+      /* Item2 properties will depend on your application concerns */
       Item: {
          id: id,
          price: price
